5a745b6352796eaa87a17fd66156620d6d40feac
s0ngkran <jinnawat8@gmail.com>
2024-08-17 02:59:41 +0700

    filter request

--- src/pages/requests.tsx
+++ src/pages/requests.tsx
@@ -1,9 +1,10 @@
 import TextHeader from "@/components/shared/textHeader";
 import RequestCard from "@/components/requests/requestCard";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import fetchAllRequest from "@/components/fetch/allRequest";
 import useRequestStore, { RequestType } from "@/store/requestStore";
+import { useSession } from "next-auth/react";
 
 export async function getServerSideProps() {
   const data = await fetchAllRequest();
@@ -21,17 +22,33 @@ export type RequestPageProps = {
 
 function RequestPage(props: RequestPageProps) {
   const { requests, setRequests } = useRequestStore();
+  const [isOnlyMyRequests, setIsOnlyMyRequests] = useState(false);
+
+  const { data: session } = useSession();
 
   useEffect(() => {
     setRequests(props.requests);
   }, [props.requests, setRequests]);
 
-  const data = requests.length > 0 ? requests : props.requests;
+  let data = requests.length > 0 ? requests : props.requests;
+
+  const me = session?.user?.oid?.toString();
+  if (isOnlyMyRequests) {
+    data = data.filter((request) => request.user_id === me);
+  }
   return (
     <div>
       <div className="flex flex-col mx-10 space-y-2">
         <div className="flex flex-row justify-between">
-          <TextHeader>Requests</TextHeader>
+          <div className="flex flex-row items-center space-x-2">
+            <TextHeader>Requests</TextHeader>
+            <div
+              onClick={() => setIsOnlyMyRequests((p) => !p)}
+              className="bg-green-600 p-1 rounded hover:cursor-pointer"
+            >
+              my requests
+            </div>
+          </div>
           <Link href="/new_request">
             <div className="bg-green-600 text-white p-2 rounded-sm">+</div>
           </Link>
